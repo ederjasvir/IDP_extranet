b361db0e90472e5d7b40935f0925f14ba4b51f58
ederjasvir <eder@netfusionperu.com>
2022-03-16 17:21:00 -0500

    Agregar archivos de proyecto.

---
+++ IDP_Extranet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IDP_Extranet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
